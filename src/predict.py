8e0db99fb5215df25e5828c1e6080db884d648c7
codewith-sameer <workwithsameer.n@gmail.com>
2024-08-04 19:44:33 +0530

    added predict.py file

---
+++ src/predict.py
@@ -0,0 +1,10 @@
+import mlflow.sklearn
+import pandas as pd
+
+# Load model
+model = mlflow.sklearn.load_model("models:/model/production")
+
+def predict(input_data):
+    input_df = pd.DataFrame(input_data)
+    predictions = model.predict(input_df)
+    return predictions
