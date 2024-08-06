bcb6b994f15121925e63332d3f04a1c4cc793431
codewith-sameer <workwithsameer.n@gmail.com>
2024-08-06 21:52:29 +0530

    Removed lint error

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ from app import app
 
 # Add the src directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                             "../src")))  
+                                             "../src")))
 
 
 @pytest.fixture
